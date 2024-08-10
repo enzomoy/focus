f39c0ad1e95468021bb2a4398229ab308f9cbf6c
Enzo <enzo.moy94@gmail.com>
2024-08-10 18:54:03 +0200

    edit project structure

---
+++ packages/backend/src/pipes/zod.pipe.ts
@@ -0,0 +1,21 @@
+import { BadRequestError } from '@common/errors/CustomError';
+import { PipeTransform } from '@nestjs/common';
+import { ZodSchema } from 'zod';
+
+export class ZodValidationPipe implements PipeTransform {
+  constructor(private schema: ZodSchema) {}
+
+  transform(value: unknown) {
+    try {
+      const parsedValue: any = this.schema.parse(value);
+      return parsedValue;
+    } catch (error) {
+      const err = error.errors[0].message.split(' ');
+      if (err.length === 1) err.push(err[0]);
+      if (error.errors[0].path.length > 0) err[0] = error.errors[0].path[0];
+
+      const str: string = err.join(' ');
+      throw new BadRequestError(str.toLowerCase());
+    }
+  }
+}
